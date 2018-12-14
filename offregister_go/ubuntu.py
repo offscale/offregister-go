278f05ccefca965926ac64a6d1062924661eed84
Samuel Marks <samuelmarks@gmail.com>
2018-12-15 00:16:04 +1100

    Upgraded default Go version

--- offregister_go/ubuntu.py
+++ offregister_go/ubuntu.py
@@ -5,7 +5,7 @@ from offregister_fab_utils.fs import append_path, cmd_avail
 
 
 def install0(arch='amd64', *args, **kwargs):
-    version = kwargs.get('GOVERSION', '1.11.2')
+    version = kwargs.get('GOVERSION', '1.11.3')
 
     if cmd_avail('go'):
         current_version = run('go version', quiet=True).rpartition(' ')[0].rpartition(' ')[2][2:]
@@ -19,7 +19,6 @@ def install0(arch='amd64', *args, **kwargs):
     run('curl -O https://storage.googleapis.com/golang/{go_tar}'.format(go_tar=go_tar))
     sudo('tar -C {install_loc} -xzf {go_tar}'.format(install_loc=install_loc, go_tar=go_tar))
     append_path('{install_loc}/go/bin:{go_path}/bin'.format(install_loc=install_loc, go_path=go_path))
-    sudo("sed -i '0,/can/{//d}' /etc/environment")
     append('/etc/environment', 'GOPATH={go_path}'.format(go_path=go_path), use_sudo=True)
     run('rm {go_tar}'.format(go_tar=go_tar))
     # run('rm -rf go*')
