8c25a3477ec70fb7731ae48230d04df96e99c426
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_go/ubuntu.py
+++ offregister_go/ubuntu.py
@@ -4,23 +4,39 @@ from fabric.operations import run, sudo
 from offregister_fab_utils.fs import append_path, cmd_avail
 
 
-def install0(arch='amd64', *args, **kwargs):
-    version = kwargs.get('GOVERSION', '1.12.9')
+def install0(arch="amd64", *args, **kwargs):
+    version = kwargs.get("GOVERSION", "1.12.9")
 
-    if cmd_avail('go'):
-        current_version = run('go version', quiet=True).rpartition(' ')[0].rpartition(' ')[2][2:]
+    if cmd_avail("go"):
+        current_version = (
+            run("go version", quiet=True).rpartition(" ")[0].rpartition(" ")[2][2:]
+        )
         if current_version == version:
-            return 'Go {current_version} already installed'.format(current_version=current_version)
+            return "Go {current_version} already installed".format(
+                current_version=current_version
+            )
 
-    go_path = kwargs.get('GOPATH', run('echo $HOME/go', quiet=True))
-    install_loc = '/usr/local'
-    go_tar = 'go{version}.{os}-{arch}.tar.gz'.format(version=version, os='linux', arch=arch)
+    go_path = kwargs.get("GOPATH", run("echo $HOME/go", quiet=True))
+    install_loc = "/usr/local"
+    go_tar = "go{version}.{os}-{arch}.tar.gz".format(
+        version=version, os="linux", arch=arch
+    )
 
-    run('curl -O https://storage.googleapis.com/golang/{go_tar}'.format(go_tar=go_tar))
-    sudo('tar -C {install_loc} -xzf {go_tar}'.format(install_loc=install_loc, go_tar=go_tar))
-    append_path('{install_loc}/go/bin:{go_path}/bin'.format(install_loc=install_loc, go_path=go_path))
-    append('/etc/environment', 'GOPATH={go_path}'.format(go_path=go_path), use_sudo=True)
-    run('rm {go_tar}'.format(go_tar=go_tar))
+    run("curl -O https://storage.googleapis.com/golang/{go_tar}".format(go_tar=go_tar))
+    sudo(
+        "tar -C {install_loc} -xzf {go_tar}".format(
+            install_loc=install_loc, go_tar=go_tar
+        )
+    )
+    append_path(
+        "{install_loc}/go/bin:{go_path}/bin".format(
+            install_loc=install_loc, go_path=go_path
+        )
+    )
+    append(
+        "/etc/environment", "GOPATH={go_path}".format(go_path=go_path), use_sudo=True
+    )
+    run("rm {go_tar}".format(go_tar=go_tar))
     # run('rm -rf go*')
-    run('mkdir -p {go_path}{subdirs}'.format(go_path=go_path, subdirs='/{bin,src}'))
-    return 'Installed:{}'.format(run('go version', quiet=True))
+    run("mkdir -p {go_path}{subdirs}".format(go_path=go_path, subdirs="/{bin,src}"))
+    return "Installed:{}".format(run("go version", quiet=True))
