ed4318bf59180e1d742d34ec52f8551005d437f3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-15 22:49:56 -0400

    [offregister_go/{__init__.py,ubuntu.py}] upgrade to latest Fabric ; [setup.py] Support Python 3

--- offregister_go/ubuntu.py
+++ offregister_go/ubuntu.py
@@ -1,41 +1,46 @@
-from fabric.contrib.files import append
-from fabric.operations import run, sudo
+from fabric.operations import run
 from offregister_fab_utils.fs import append_path, cmd_avail
+from patchwork.files import append
 
 
 def install0(arch="amd64", *args, **kwargs):
     version = kwargs.get("GOVERSION", "1.12.9")
 
-    if cmd_avail("go"):
+    if cmd_avail(c, "go"):
         current_version = (
-            run("go version", quiet=True).rpartition(" ")[0].rpartition(" ")[2][2:]
+            c.run("go version", hide=True)
+            .stdout.rpartition(" ")[0]
+            .rpartition(" ")[2][2:]
         )
         if current_version == version:
             return "Go {current_version} already installed".format(
                 current_version=current_version
             )
 
-    go_path = kwargs.get("GOPATH", run("echo $HOME/go", quiet=True))
+    go_path = kwargs.get("GOPATH", c.run("echo $HOME/go", hide=True).stdout.rstrip())
     install_loc = "/usr/local"
     go_tar = "go{version}.{os}-{arch}.tar.gz".format(
         version=version, os="linux", arch=arch
     )
 
-    run("curl -O https://storage.googleapis.com/golang/{go_tar}".format(go_tar=go_tar))
-    sudo(
+    c.run(
+        "curl -O https://storage.googleapis.com/golang/{go_tar}".format(go_tar=go_tar)
+    )
+    c.sudo(
         "tar -C {install_loc} -xzf {go_tar}".format(
             install_loc=install_loc, go_tar=go_tar
         )
     )
     append_path(
+        c,
         "{install_loc}/go/bin:{go_path}/bin".format(
             install_loc=install_loc, go_path=go_path
-        )
+        ),
     )
     append(
         "/etc/environment", "GOPATH={go_path}".format(go_path=go_path), use_sudo=True
     )
-    run("rm {go_tar}".format(go_tar=go_tar))
-    # run('rm -rf go*')
-    run("mkdir -p {go_path}{subdirs}".format(go_path=go_path, subdirs="/{bin,src}"))
-    return "Installed:{}".format(run("go version", quiet=True))
+    c.run("rm {go_tar}".format(go_tar=go_tar))
+    # c.run('rm -rf go*')
+    c.run("mkdir -p {go_path}{subdirs}".format(go_path=go_path, subdirs="/{bin,src}"))
+    return "Installed:{}".format(run("go version", hide=True).stdout)
